383d6330effa3d247d76505b55b7766bb604e9c9
Jimmy Zhen <hypeway@me.com>
2019-05-19 04:28:12 -0700

    Add eslint config

---
+++ .eslintrc.js
@@ -0,0 +1,16 @@
+module.exports = {
+  "extends": "airbnb",
+  "plugins": [
+    "react-hooks",
+  ],
+  "rules": {
+    "jsx-a11y/label-has-for": "off",
+    "jsx-a11y/label-has-associated-control": "off",
+    "react-hooks/rules-of-hooks": "error",
+    "react-hooks/exhaustive-deps": "warn"
+  },
+  "env": {
+    "browser": true,
+    "jest": true
+  }
+};

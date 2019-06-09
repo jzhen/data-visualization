0ae5ade2b33d90e5ec983ccc25735386c7c783fc
Jimmy Zhen <hypeway@me.com>
2019-06-09 12:12:44 -0700

    Inmplemented routes in place of default CRA content

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,22 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Router, Route, Switch } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import 'bootstrap';
+import Header from './Components/Header/header';
+import Homepage from './Components/Home/home';
+import Footer from './Components/Footer/footer';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router history={createBrowserHistory()}>
+      <div className="App">
+        <Header />
+        <Switch>
+          <Route path="/" exact component={Homepage} />
+        </Switch>
+        <Footer />
+      </div>
+    </Router>
   );
 }
 

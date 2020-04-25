4439fad0aa35bb1f9384695c2e1feb9bc3f7d685
Jimmy Zhen <hypeway@me.com>
2020-04-25 11:04:17 -0700

    Initial commit for fontawesome config

---
+++ src/lib/fontawesome.js
@@ -0,0 +1,10 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faChartBar, faGlobeAmericas, faMagic } from '@fortawesome/free-solid-svg-icons';
+
+const fontawesomeIconSet = library.add(
+  faChartBar,
+  faGlobeAmericas,
+  faMagic,
+);
+
+export default fontawesomeIconSet;

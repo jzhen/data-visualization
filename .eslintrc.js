a4aab5ca33b4f02edf5431b07bfb918e33a37057
Jimmy Zhen <hypeway@me.com>
2020-12-22 16:49:42 -0800

    Updated eslinting settings

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,11 +1,9 @@
 module.exports = {
-  "extends": "airbnb",
-  "plugins": [
-    "react-hooks",
-  ],
-  "rules": {
-    'max-len': [2, {'code': 110, 'tabWidth': 2, 'ignoreUrls': true}],
-    'react/jsx-filename-extension': ['error', { 'extensions': ['.js', '.jsx'] }],
+  extends: ['airbnb', 'plugin:prettier/recommended'],
+  plugins: ['react-hooks'],
+  rules: {
+    'max-len': [2, { code: 110, tabWidth: 2, ignoreUrls: true }],
+    'react/jsx-filename-extension': ['error', { extensions: ['.js', '.jsx'] }],
     'global-require': 'off',
     'no-console': 'off',
     'import/no-extraneous-dependencies': 'off',
@@ -22,25 +20,25 @@ module.exports = {
     'react/jsx-props-no-spreading': 'off',
     'react/jsx-fragments': 'off',
     'react/prefer-stateless-function': 'off',
-    "jsx-a11y/label-has-for": "off",
-    "jsx-a11y/label-has-associated-control": "off",
-    "react-hooks/rules-of-hooks": "error",
-    "react-hooks/exhaustive-deps": "warn"
+    'jsx-a11y/label-has-for': 'off',
+    'jsx-a11y/label-has-associated-control': 'off',
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
   },
-  "env": {
-    "browser": true,
-    "commonjs": true,
-    "es6": true,
-    "node": true,
-    "jest": true
+  env: {
+    browser: true,
+    commonjs: true,
+    es6: true,
+    node: true,
+    jest: true,
   },
-  "parserOptions": {
-    "ecmaVersion": 6,
-    "sourceType": "module",
-    "ecmaFeatures": {
-        "jsx": true,
-        "classes": true,
-        "defaultParams": true
-    }
-  }
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
+      classes: true,
+      defaultParams: true,
+    },
+  },
 };

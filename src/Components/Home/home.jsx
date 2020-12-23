8f0c8bf92a5cf4109de9f2b804cf2565ee603603
Jimmy Zhen <hypeway@me.com>
2020-12-22 16:57:06 -0800

    Updated main page UIs

--- src/Components/Home/home.jsx
+++ src/Components/Home/home.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faGithub, faLinkedin } from '@fortawesome/free-brands-svg-icons';
 
 function Homepage() {
   return (
     <main role="main">
-
       <section className="jumbotron text-center">
         <div className="container">
-          <h1 className="jumbotron-heading">👨‍💻 Experiments in visualizing data</h1>
-          <p className="lead text-muted">Something short and leading about the collection below—its contents, the creator, etc. Make it short and sweet, but not too short so folks don’t simply skip over it entirely.</p>
+          <h1 className="jumbotron-heading">
+            👨‍💻 Experiments in visualizing data
+          </h1>
+          <p className="lead text-muted">
+            A personal exploration of data visualization using different open
+            source charting libraries in the attempt to understand as well as to
+            compare their strength and weakness.
+          </p>
           <p>
-            <Link to="/" className="btn btn-primary mx-1 my-2">Main call to action</Link>
-            <Link to="/" className="btn btn-secondary mx-1 my-2">Secondary action</Link>
+            <a
+              href="https://github.com/jzhen/data-visualization"
+              className="btn btn-primary btn-lg mx-2 my-2"
+            >
+              <FontAwesomeIcon icon={faGithub} />
+              <span className="ml-2">Source</span>
+            </a>
+            <a
+              href="https://www.linkedin.com/in/jimmy-zhen-6612691/"
+              className="btn btn-secondary btn-lg mx-2 my-2"
+            >
+              <FontAwesomeIcon icon={faLinkedin} />
+              <span className="ml-2">Connect</span>
+            </a>
           </p>
         </div>
       </section>
@@ -22,9 +40,14 @@ function Homepage() {
           <div className="row">
             <div className="col-md-4">
               <div className="card mb-4 shadow-sm">
-                <div width="100%" height="225" background="#55595c" color="#eceeef" className="card-img-top"><span>Thumbnail</span></div>
+                <div width="100%" height="225" background="#55595c" color="#eceeef" className="card-img-top">
+                  <span>Thumbnail</span>
+                </div>
                 <div className="card-body">
-                  <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural lead-in to additional
+                    content. This content is a little bit longer.
+                  </p>
                   <div className="d-flex justify-content-between align-items-center">
                     <div className="btn-group">
                       <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
@@ -37,9 +60,14 @@ function Homepage() {
             </div>
             <div className="col-md-4">
               <div className="card mb-4 shadow-sm">
-                <div width="100%" height="225" background="#55595c" color="#eceeef" className="card-img-top"><span>Thumbnail</span></div>
+                <div width="100%" height="225" background="#55595c" color="#eceeef" className="card-img-top">
+                  <span>Thumbnail</span>
+                </div>
                 <div className="card-body">
-                  <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural lead-in to additional
+                    content. This content is a little bit longer.
+                  </p>
                   <div className="d-flex justify-content-between align-items-center">
                     <div className="btn-group">
                       <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
@@ -52,55 +80,14 @@ function Homepage() {
             </div>
             <div className="col-md-4">
               <div className="card mb-4 shadow-sm">
-                <div width="100%" height="225" background="#55595c" color="#eceeef" className="card-img-top"><span>Thumbnail</span></div>
-                <div className="card-body">
-                  <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small className="text-muted">9 mins</small>
-                  </div>
+                <div width="100%" height="225" background="#55595c" color="#eceeef" className="card-img-top">
+                  <span>Thumbnail</span>
                 </div>
-              </div>
-            </div>
-
-            <div className="col-md-4">
-              <div className="card mb-4 shadow-sm">
-                <div width="100%" height="225" background="#55595c" color="#eceeef" className="card-img-top"><span>Thumbnail</span></div>
                 <div className="card-body">
-                  <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small className="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className="card mb-4 shadow-sm">
-                <div width="100%" height="225" background="#55595c" color="#eceeef" className="card-img-top"><span>Thumbnail</span></div>
-                <div className="card-body">
-                  <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                  <div className="d-flex justify-content-between align-items-center">
-                    <div className="btn-group">
-                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
-                    </div>
-                    <small className="text-muted">9 mins</small>
-                  </div>
-                </div>
-              </div>
-            </div>
-            <div className="col-md-4">
-              <div className="card mb-4 shadow-sm">
-                <div width="100%" height="225" background="#55595c" color="#eceeef" className="card-img-top"><span>Thumbnail</span></div>
-                <div className="card-body">
-                  <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural lead-in to additional
+                    content. This content is a little bit longer.
+                  </p>
                   <div className="d-flex justify-content-between align-items-center">
                     <div className="btn-group">
                       <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
@@ -114,9 +101,14 @@ function Homepage() {
 
             <div className="col-md-4">
               <div className="card mb-4 shadow-sm">
-                <div width="100%" height="225" background="#55595c" color="#eceeef" className="card-img-top"><span>Thumbnail</span></div>
+                <div width="100%" height="225" background="#55595c" color="#eceeef" className="card-img-top">
+                  <span>Thumbnail</span>
+                </div>
                 <div className="card-body">
-                  <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural lead-in to additional
+                    content. This content is a little bit longer.
+                  </p>
                   <div className="d-flex justify-content-between align-items-center">
                     <div className="btn-group">
                       <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
@@ -129,9 +121,14 @@ function Homepage() {
             </div>
             <div className="col-md-4">
               <div className="card mb-4 shadow-sm">
-                <div width="100%" height="225" background="#55595c" color="#eceeef" className="card-img-top"><span>Thumbnail</span></div>
+                <div width="100%" height="225" background="#55595c" color="#eceeef" className="card-img-top">
+                  <span>Thumbnail</span>
+                </div>
                 <div className="card-body">
-                  <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural lead-in to additional
+                    content. This content is a little bit longer.
+                  </p>
                   <div className="d-flex justify-content-between align-items-center">
                     <div className="btn-group">
                       <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
@@ -144,9 +141,75 @@ function Homepage() {
             </div>
             <div className="col-md-4">
               <div className="card mb-4 shadow-sm">
-                <div width="100%" height="225" background="#55595c" color="#eceeef" className="card-img-top"><span>Thumbnail</span></div>
+                <div width="100%" height="225" background="#55595c" color="#eceeef" className="card-img-top">
+                  <span>Thumbnail</span>
+                </div>
                 <div className="card-body">
-                  <p className="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural lead-in to additional
+                    content. This content is a little bit longer.
+                  </p>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="btn-group">
+                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                      <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                    </div>
+                    <small className="text-muted">9 mins</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+
+            <div className="col-md-4">
+              <div className="card mb-4 shadow-sm">
+                <div width="100%" height="225" background="#55595c" color="#eceeef" className="card-img-top">
+                  <span>Thumbnail</span>
+                </div>
+                <div className="card-body">
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural lead-in to additional
+                    content. This content is a little bit longer.
+                  </p>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="btn-group">
+                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                      <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                    </div>
+                    <small className="text-muted">9 mins</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card mb-4 shadow-sm">
+                <div width="100%" height="225" background="#55595c" color="#eceeef" className="card-img-top">
+                  <span>Thumbnail</span>
+                </div>
+                <div className="card-body">
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural lead-in to additional
+                    content. This content is a little bit longer.
+                  </p>
+                  <div className="d-flex justify-content-between align-items-center">
+                    <div className="btn-group">
+                      <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
+                      <button type="button" className="btn btn-sm btn-outline-secondary">Edit</button>
+                    </div>
+                    <small className="text-muted">9 mins</small>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="col-md-4">
+              <div className="card mb-4 shadow-sm">
+                <div width="100%" height="225" background="#55595c" color="#eceeef" className="card-img-top">
+                  <span>Thumbnail</span>
+                </div>
+                <div className="card-body">
+                  <p className="card-text">
+                    This is a wider card with supporting text below as a natural lead-in to additional
+                    content. This content is a little bit longer.
+                  </p>
                   <div className="d-flex justify-content-between align-items-center">
                     <div className="btn-group">
                       <button type="button" className="btn btn-sm btn-outline-secondary">View</button>
@@ -160,7 +223,6 @@ function Homepage() {
           </div>
         </div>
       </div>
-
     </main>
   );
 }

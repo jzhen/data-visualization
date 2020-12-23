2b31fd41464e145c75c6d85bf60ba274d1cf1101
Jimmy Zhen <hypeway@me.com>
2020-12-22 16:56:28 -0800

    Updated header UIs

--- src/Components/Header/header.jsx
+++ src/Components/Header/header.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faChartBar } from '@fortawesome/free-solid-svg-icons';
 
 function Header() {
   return (
@@ -11,16 +12,22 @@ function Header() {
             <div className="col-sm-8 col-md-7 py-4">
               <h4 className="text-white">About</h4>
               <p className="text-muted">
-                This is a playground for me to explore data visualization on various
-                datasets found at&nbsp;
-                <a href="https://datacatalog.worldbank.org/">The World Bank Data Catalog</a>
+                This is a playground for me to explore data visualization on
+                various datasets found at&nbsp;
+                <a href="https://datacatalog.worldbank.org/">
+                  The World Bank Data Catalog
+                </a>
                 .
               </p>
             </div>
             <div className="col-sm-4 offset-md-1 py-4">
               <h4 className="text-white">Contact</h4>
               <ul className="list-unstyled">
-                <li><Link to="mailto:me@example.com" className="text-white">Email me</Link></li>
+                <li>
+                  <a href="mailto:hypeway@me.com" className="text-white">
+                    Email me
+                  </a>
+                </li>
               </ul>
             </div>
           </div>
@@ -29,10 +36,18 @@ function Header() {
       <div className="navbar navbar-dark bg-dark shadow-sm">
         <div className="container d-flex justify-content-between">
           <Link to="/" className="navbar-brand d-flex align-items-center">
-            <FontAwesomeIcon icon="chart-bar" />
-            <strong>Data Visualization Playground</strong>
+            <FontAwesomeIcon icon={faChartBar} />
+            <strong className="ml-2">Data Visualization Playground</strong>
           </Link>
-          <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarHeader" aria-controls="navbarHeader" aria-expanded="false" aria-label="Toggle navigation">
+          <button
+            className="navbar-toggler"
+            type="button"
+            data-toggle="collapse"
+            data-target="#navbarHeader"
+            aria-controls="navbarHeader"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
             <span className="navbar-toggler-icon" />
           </button>
         </div>

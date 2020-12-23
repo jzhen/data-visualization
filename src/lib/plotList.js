78f60f2beac086dd4644f1868e88440840f4ae44
Jimmy Zhen <hypeway@me.com>
2020-12-22 18:22:01 -0800

    Initial commit for mock plots metadata

---
+++ src/lib/plotList.js
@@ -0,0 +1,68 @@
+/* eslint-disable */
+const plots = [
+  {
+    id: '001',
+    label: 'CO2 Emission',
+    description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+    library_name: 'Formidable Victory',
+    library_url: 'https://formidable.com/open-source/victory/',
+  },
+  {
+    id: '002',
+    label: 'CO2 Emission',
+    description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+    library_name: 'Formidable Victory',
+    library_url: 'https://formidable.com/open-source/victory/',
+  },
+  {
+    id: '003',
+    label: 'CO2 Emission',
+    description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+    library_name: 'Formidable Victory',
+    library_url: 'https://formidable.com/open-source/victory/',
+  },
+  {
+    id: '004',
+    label: 'CO2 Emission',
+    description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+    library_name: 'Formidable Victory',
+    library_url: 'https://formidable.com/open-source/victory/',
+  },
+  {
+    id: '005',
+    label: 'CO2 Emission',
+    description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+    library_name: 'Formidable Victory',
+    library_url: 'https://formidable.com/open-source/victory/',
+  },
+  {
+    id: '006',
+    label: 'CO2 Emission',
+    description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+    library_name: 'Formidable Victory',
+    library_url: 'https://formidable.com/open-source/victory/',
+  },
+  {
+    id: '007',
+    label: 'CO2 Emission',
+    description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+    library_name: 'Formidable Victory',
+    library_url: 'https://formidable.com/open-source/victory/',
+  },
+  {
+    id: '008',
+    label: 'CO2 Emission',
+    description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+    library_name: 'Formidable Victory',
+    library_url: 'https://formidable.com/open-source/victory/',
+  },
+  {
+    id: '009',
+    label: 'CO2 Emission',
+    description: 'This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.',
+    library_name: 'Formidable Victory',
+    library_url: 'https://formidable.com/open-source/victory/',
+  },
+];
+
+export default plots;
